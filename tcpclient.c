d29258253da8521f2408a1092dd9a391e43fd640
Valentyn Faychuk <vfaychuk@localhost.localdomain>
2019-11-03 16:06:20 -0500

    Added guacamole handshake support

--- tcpclient.c
+++ tcpclient.c
@@ -30,6 +30,35 @@
 #include <string.h>
 #include <stdio.h>
 
+int hardcoded_guacamole_handshake(SOCKET socket_peer) {
+    const char *select_instruction = "6.select,3.vnc;";
+    const char *connect_instruction = "4.size,4.1024,3.768,2.96;5.audio,9.audio/ogg;5.video;5.image,9.image/png,10.image/jpeg;7.connect,9.localhost,4.5900,0.,0.,0.;";
+    int bytes_sent = send(socket_peer, select_instruction, strlen(select_instruction), 0);
+    printf("\nSent (%d bytes): %s", bytes_sent, select_instruction);
+
+    char read[4096];
+    int bytes_received = recv(socket_peer, read, 4096, 0);
+    if (bytes_received < 1) {
+        printf("Connection closed by peer.\n");
+        return 1;
+    }
+    printf("\n\nReceived (%d bytes): %.*s", 
+        bytes_received, bytes_received, read);
+
+    bytes_sent = send(socket_peer, connect_instruction, strlen(connect_instruction), 0);
+    printf("\n\nSent (%d bytes): %s", bytes_sent, connect_instruction);
+
+    bytes_received = recv(socket_peer, read, 4096, 0);
+    if (bytes_received < 1) {
+        printf("Connection closed by peer.\n");
+        return 1;
+    }
+    printf("\n\nReceived (%d bytes): %.*s\n\n", 
+        bytes_received, bytes_received, read);
+        
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
 #if defined(_WIN32)
     WSADATA d;
@@ -76,6 +105,10 @@ int main(int argc, char *argv[]) {
     freeaddrinfo(peer_address);
     
     printf("Connected.\n");
+    printf("Relizing guacamole handshake.\n");
+    if (hardcoded_guacamole_handshake(socket_peer)) {
+        return 1;
+    }
     printf("To send data, enter text followed by enter.\n");
     while(1) {
         fd_set reads;
